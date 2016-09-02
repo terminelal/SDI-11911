48eb3a4f7421ce5d69611108dd324150f6b9266a
aperezbar <aldebaran_1995@hotmail.com>
2016-09-01 23:38:44 -0500

    Proyecto 149392

---
+++ DiegoPozoProyecto1/DiegoPozo_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/DiegoPozo_ws/src
+# Build directory: /home/ros/DiegoPozo_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(dp_pkg)
---
+++ DiegoPozoProyecto1/DiegoPozo_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/ros/DiegoPozo_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/ros/DiegoPozo_ws/devel;/opt/ros/indigo".split(';') if "/home/ros/DiegoPozo_ws/devel;/opt/ros/indigo" != "" else []
