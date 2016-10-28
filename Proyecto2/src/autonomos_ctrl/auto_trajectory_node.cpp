7b372677500253e59c7012aa1ee37bbc23af6bc9
HHugoHH <hugo.huipet@gmail.com>
2016-10-27 23:49:59 -0500

    First Try at Parallel Parking

---
+++ Proyecto2/src/autonomos_ctrl/auto_trajectory_node.cpp
@@ -0,0 +1,184 @@
+#include <ros/ros.h>
+#include "std_msgs/String.h"
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
+#include <stdlib.h>
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/QR>
+#include <Eigen/LU>
+#include <math.h>
+#include <stdio.h>
+//#include <turtlesim/Pose.h>
+#include <Eigen/Geometry>
+#include "gazebo_msgs/LinkStates.h"
+// #include <LinkStates.h>
+#include <gazebo_msgs/GetJointProperties.h>
+
+using namespace Eigen;
+
+//For geometry_msgs::Twist using:
+// 		dummy.linear.x
+// 		dummy.linear.y
+// 		dummy.angular.z
+geometry_msgs::Twist rposition;
+geometry_msgs::Twist target_position;
+
+//rate_hz assignment
+double rate_hz = 5;
+
+//Assign the position of the robot (from other topic) to robot_position.
+//Assuming the topic that generate the robot position uses geometry_msgs::Twist
+//and the information is in *.linear. This might need to be modifed
+/*void getRobotPose(const gazebo_msgs::LinkStates& msg) {
+
+	// msg.name[1]
+	rposition.position.x = msg.pose[1].position.x;
+    rposition.position.y = msg.pose[1].position.y;
+    rposition.orientation.x = msg.pose[1].orientation.x; 
+    rposition.orientation.y = msg.pose[1].orientation.y; 
+    rposition.orientation.z = msg.pose[1].orientation.z; 
+    rposition.orientation.w = msg.pose[1].orientation.w; 
+}*/
+
+//Assign the position of the target (from other topic) to target_position.
+//Assuming the topic that generate the robot position uses geometry_msgs::Twist
+//and the information is in *.linear. This might need to be modifed
+void getTargetPose(const geometry_msgs::Twist& msg) {
+	target_position.linear.x = msg.linear.x;
+	target_position.linear.y = msg.linear.y;
+    target_position.angular.z = msg.angular.z; 
+}
+
+//Function to determine if the goal is still far.
+//Variables epsilon_x and epsilon_y used to determined if the goal has been reach
+bool isGoalFar(geometry_msgs::Twist p_start, geometry_msgs::Twist p_goal) {
+	double epsilon_x, epsilon_y;
+	epsilon_x = .2; 
+	epsilon_y = .2;
+	double distance_x = abs(p_goal.linear.x - p_start.linear.x);
+	double distance_y = abs(p_goal.linear.y - p_start.linear.y);
+	if (distance_x > epsilon_x || distance_y > epsilon_y)
+		return true;
+	else
+		return false;
+}
+
+//Function to generate a Linear Constant Velocity from robot's position to the target's position
+geometry_msgs::Twist generateConstantVelocity(double constant_speed, geometry_msgs::Twist p_start, geometry_msgs::Twist p_goal){
+
+    // Compute direction to goal
+	Vector3d p_start_vector(p_start.linear.x,p_start.linear.y,p_start.angular.z);
+	Vector3d p_goal_vector(p_goal.linear.x, p_goal.linear.y, p_goal.angular.z);
+	// Vector3d goal_direction_vector = p_goal_vector-p_start_vector;
+	Vector3d goal_direction_vector = p_start_vector - p_goal_vector;
+
+    // Compute speed in the direction to goal
+	Vector3d velocity_vector = constant_speed * (goal_direction_vector/ goal_direction_vector.norm());
+
+	geometry_msgs::Twist velocity;
+
+	velocity.linear.x = velocity_vector.x();
+	velocity.linear.y = velocity_vector.y();
+	velocity.angular.z = velocity_vector.z();
+
+	return velocity;
+}
+
+// Function to keep velocity under the allowed robot limits
+geometry_msgs::Twist boundVelocity(geometry_msgs::Twist velocity) {
+
+	double max_linear_speed = 30;
+	double min_linear_speed = 0;
+	double max_angular_speed = M_PI*4;
+	double min_angular_speed = M_PI/16;
+
+	if (velocity.linear.x > max_linear_speed)
+		velocity.linear.x = max_linear_speed;
+	else if (velocity.linear.x < -max_linear_speed)
+		velocity.linear.x = -max_linear_speed;
+	if (velocity.linear.y > max_linear_speed)
+		velocity.linear.y = max_linear_speed;
+	else if (velocity.linear.y < -max_linear_speed)
+		velocity.linear.y = -max_linear_speed;
+	if (velocity.angular.z > max_angular_speed)
+		velocity.angular.z = max_angular_speed;
+	else if (velocity.angular.z < -max_angular_speed)
+		velocity.angular.z = -max_angular_speed;
+
+    // Lower speed bounds
+	if (velocity.linear.x > 0 && velocity.linear.x < min_linear_speed)
+		velocity.linear.x = min_linear_speed;
+	else if (velocity.linear.x < 0 && velocity.linear.x > -min_linear_speed)
+		velocity.linear.x = -min_linear_speed;
+	if (velocity.linear.y > 0 && velocity.linear.y<min_linear_speed)
+		velocity.linear.y = min_linear_speed;
+	else if (velocity.linear.y < 0 && velocity.linear.y > -min_linear_speed)
+		velocity.linear.y = -min_linear_speed;
+	if (velocity.angular.z >0 && velocity.angular.z < min_angular_speed)
+		velocity.angular.z = min_angular_speed;
+	else if (velocity.angular.z < 0 && velocity.linear.z > - min_angular_speed )
+		velocity.angular.z = -min_angular_speed;
+
+}
+int main(int argc, char **argv){
+	ros::init(argc,argv,"turtle_trajectory_node");
+	ros::NodeHandle nh;
+	ROS_INFO_STREAM("turtle_trajectory_node initialized");
+	ROS_INFO_STREAM(ros::this_node::getName());
+	 //Publish to the turtle topic "/turtle1/cmd_vel at rate_hz"
+	ros::Publisher pub_vel_turtle = nh.advertise<geometry_msgs::Twist>("/target_vel_topic", rate_hz);
+	//Twist variable to publish velocity (trajectories)
+	geometry_msgs::Twist desired_velocity;
+	double tiempo = 0;
+
+    //define the max speed
+	double cruise_speed = 30;
+
+    //define the rate
+	ros::Rate rate(rate_hz);
+	int counter=0;
+	while (ros::ok()) { //ESTACIONAMIENTO DEL LADO DERECHO
+		if(counter < 50) { //AVANZA UNOS METROS
+			ROS_INFO_STREAM("FORWARD");
+			desired_velocity.linear.x = 10;
+			desired_velocity.linear.y = 0; // dist
+			desired_velocity.angular.z = 0;
+		}else{
+			//STAHP //DETENTE
+			if(counter < 80 || counter > 250) { //DETENERSE EN EL MOMENTO DE VER EL ESPACIO Y ESTAR PARALELO AL SEGUNDO AUTO (80) Y DETENERESE AL FINAL TOTALMENTE (>250) 
+				ROS_INFO_STREAM("STOP!");
+				desired_velocity.linear.x = 0;
+				desired_velocity.linear.y = 0; // dist
+				desired_velocity.angular.z = 0;
+			}else{
+				if(counter < 160 ){	//ESTE MOMENTO SE ESTA METIENDO EN EL ESPACIO
+					ROS_INFO_STREAM("RIGHT AND BACKWARD!");
+					desired_velocity.linear.x = -5; //EN REVERSA SEGúN
+					desired_velocity.linear.y = 0; // dist
+					desired_velocity.angular.z = 0;//TODO: Modificar el angulo para q las ruedas apunten a la derecha max
+				}
+				else{
+					if(counter < 240 ){ //SE ALINEA EN EL ESPACIO
+						ROS_INFO_STREAM("LEFT AND BACKWARD!");
+						desired_velocity.linear.x = -5; //EN REVERSA SEGúN
+						desired_velocity.linear.y = 0; // dist
+						desired_velocity.angular.z = 0;// TODO: Ahora modificar el angulo para que las ruedas apunten hacia la izquierda Max
+					}
+				}
+			}
+			
+		}
+		
+		ROS_INFO_STREAM("counter "<<counter);
+		counter++;
+		//publish the new velocity
+		pub_vel_turtle.publish(desired_velocity);
+		
+		ros::spinOnce();
+		rate.sleep();
+        tiempo+=(1/rate_hz); 
+	}
+	return 0;
+}
+
---
+++ Proyecto2/src/autonomos_ctrl/auto_velocity_node.cpp
@@ -0,0 +1,162 @@
+#include <ros/ros.h>
+#include "std_msgs/String.h"
+#include <geometry_msgs/Twist.h>
+#include <stdlib.h>
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/QR>
+#include <Eigen/LU>
+#include <math.h>
+#include <stdio.h>
+//#include <turtlesim/Pose.h>
+#include <Eigen/Geometry>
+#include "gazebo_msgs/ApplyJointEffort.h"
+#include "gazebo_msgs/GetJointProperties.h"
+#include <math.h>
+
+using namespace Eigen;
+
+//For geometry_msgs::Twist using:
+// 		dummy.linear.x
+// 		dummy.linear.y
+// 		dummy.angular.z
+geometry_msgs::Twist robot_position;
+geometry_msgs::Twist velocity_msg;
+
+ros::ServiceClient clientSteer;
+gazebo_msgs::GetJointProperties joint_msg[1];
+
+
+//rate_hz assignment
+double rate_hz = 5;
+
+// Transform robot velocities (x,y,w) to motor velocities (m1,m2,m3,m4)
+double* getMotorValue(double x_velocity, double y_velocity, double w_velocity){
+    
+    // double R = 1;
+    double r = 3.4;
+    
+    // double Rx = 
+    double velWMod = 0.0;
+
+    double factor_velocidad = (0.02)/2;
+    
+    clientSteer.call(joint_msg[0]);
+    if(joint_msg[0].response.success == 1) {
+	printf("\nposicion steer_joint (radianes): %f", joint_msg[0].response.position[0]);
+	double pos = joint_msg[0].response.position[0];
+	
+	if(w_velocity > 0){ // izquierda
+		if(pos<w_velocity)
+			velWMod = w_velocity;
+		else if(pos>w_velocity)
+			velWMod = -w_velocity;
+		else
+			velWMod=0.0;
+	}
+	else if(w_velocity < 0){ // derecha 
+		if(pos<w_velocity)
+			velWMod = -w_velocity;
+		else if(pos>w_velocity)
+			velWMod = w_velocity;	
+		else
+			velWMod=0.0;
+	}
+	else {
+		velWMod=0.0;
+	}
+    }
+    
+    double velMots[3];
+    
+    printf("\nvelocidad: %f, angulo: %f", x_velocity, w_velocity);
+    
+    velMots[0] = x_velocity*factor_velocidad;
+    velMots[1] = x_velocity*factor_velocidad;
+    
+    velMots[2] = velWMod;
+    
+    return velMots;
+}
+
+void get_vel_vec(const geometry_msgs::Twist& msg) {
+	velocity_msg.linear.x = msg.linear.x;
+	velocity_msg.linear.y = msg.linear.y;
+	velocity_msg.angular.z = msg.angular.z; 
+}
+
+
+int main(int argc, char **argv){
+	ros::init(argc,argv,"auto_velocity_node");
+	ros::NodeHandle nh;
+	ROS_INFO_STREAM("auto_velocity_node initialized");
+	ROS_INFO_STREAM(ros::this_node::getName());
+	
+	// Suscribe to Gazebo service ApplyJointEffor
+	ros::ServiceClient client = nh.serviceClient<gazebo_msgs::ApplyJointEffort>("/gazebo/apply_joint_effort");
+	gazebo_msgs::ApplyJointEffort eff_msg[3];
+
+	// Suscribe to Gazebo service get_joint_properties
+	clientSteer = nh.serviceClient<gazebo_msgs::GetJointProperties>("/gazebo/get_joint_properties");	
+	joint_msg[0].request.joint_name = "steer_joint";
+	
+	ros::Subscriber sub_vel = nh.subscribe("/target_vel_topic", 100, &get_vel_vec);
+
+	double tiempo = 0;
+
+    	//define the max speed
+	double cruise_speed = 50;
+
+	//define the rate
+	ros::Rate rate(rate_hz);
+	ros::Time start_time ;
+	ros::Duration duration ;
+
+	double effort[3];
+	
+	while (ros::ok())
+	{
+		double* velMots = getMotorValue(velocity_msg.linear.x,velocity_msg.linear.y,velocity_msg.angular.z);
+		effort [0] = velMots[0];
+		effort [1] = velMots[1];
+		effort [2] = velMots[2];
+	  
+		if(client.exists()){
+			
+			start_time.sec = 0;
+			start_time.nsec = 0;
+			duration.sec = 0;
+			duration.nsec = (1/rate_hz)*pow(10,9);
+			
+			printf("\neffort: %f %f %f\n", effort[0],effort[1],effort[2]);
+			
+			// Wheel-Joint 1
+			eff_msg[0].request.joint_name = "back_right_wheel_joint";
+			eff_msg[0].request.duration = duration;
+			eff_msg[0].request.effort = effort[0];
+			eff_msg[0].request.start_time = start_time;
+
+			// Wheel-Joint 2
+			eff_msg[1].request.joint_name = "back_left_wheel_joint";
+			eff_msg[1].request.duration = duration;
+			eff_msg[1].request.effort = effort[1];
+			eff_msg[1].request.start_time = start_time;
+
+			// Wheel-Joint 3
+			eff_msg[2].request.joint_name = "steer_joint";
+			eff_msg[2].request.duration = duration;
+			eff_msg[2].request.effort = effort[2];
+			eff_msg[2].request.start_time = start_time;
+
+			client.call(eff_msg[0]);
+			client.call(eff_msg[1]);
+			client.call(eff_msg[2]);
+
+			ROS_INFO_STREAM("\nJoints ==> 1: " << ((eff_msg[0].response.success == 1) ? "TRUE" : "FALSE") << " 2: " << ((eff_msg[1].response.success == 1) ? "TRUE" : "FALSE") << " 3: " << ((eff_msg[2].response.success == 1) ? "TRUE" : "FALSE") );
+		}																																																																																												
+		
+		ros::spinOnce();
+		rate.sleep();
+    }
+    return 0;
+}
